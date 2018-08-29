2bf00c5aeb0eb3f0c52f60395d70375ba3623794
Dan Green <danon0404@gmail.com>
2018-08-29 22:12:25 +0700

    v1.2.1

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const noRuntime = require('./no-runtime');
-
-module.exports = () => {
+module.exports = (_, options) => {
 
 	const {
-		presets,
-		plugins
-	} = noRuntime();
+		commonjs,
+		transformRuntime
+	} = Object.assign({}, options, {
+		commonjs:         false,
+		transformRuntime: true
+	});
+	const presets = [
+		['@babel/preset-env']
+	];
+	const plugins = [
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-import-meta',
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-json-strings',
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		'@babel/plugin-proposal-function-sent',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-proposal-numeric-separator',
+		'@babel/plugin-proposal-throw-expressions',
+		'@babel/plugin-proposal-export-default-from',
+		'@babel/plugin-proposal-async-generator-functions'
+	];
 
-	plugins.unshift(['@babel/plugin-transform-runtime', { corejs: 2 }]);
+	if (!commonjs) {
+		presets[0].push({ modules: false });
+	}
+
+	if (transformRuntime) {
+		plugins.unshift(['@babel/plugin-transform-runtime', { corejs: 2 }]);
+	}
 
 	return {
 		presets,
